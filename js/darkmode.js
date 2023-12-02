4c72efd8a07a2224a32a48f468cbb73aee75d749
Máximo <maximogoitia277@gmail.com>
2023-12-02 13:11:21 -0300

    modo oscuro en sobre mi

--- js/darkmode.js
+++ js/darkmode.js
@@ -8,20 +8,37 @@ if (ingresar == true) {
     var div6 = document.getElementsByClassName("texto2")[0];
     switchButton.addEventListener("change", function () {
     if (this.checked) {
-        div1.style.backgroundColor = "#1E1927"
-        div2.style.backgroundColor = "#352C48"
+        div1.style.backgroundColor = "#1E1927";
+        div2.style.backgroundColor = "#352C48";
         div2.style.boxShadow = "0 0px 10px 5px white";
-        div3.style.backgroundColor = "white"
-        div4.style.backgroundColor = "white"
-        div5.style.color = "#352C48"
-        div6.style.color = "#352C48"
+        div3.style.backgroundColor = "white";
+        div4.style.backgroundColor = "white";
+        div5.style.color = "#352C48";
+        div6.style.color = "#352C48";
     } else {
         div1.style.backgroundColor = "white"
         div2.style.backgroundColor = "white"
         div2.style.boxShadow = "0 0px 10px 5px black";
         div3.style.backgroundColor = "#1e1927"
         div4.style.backgroundColor = "#1e1927"
-        div5.style.color = "white"
-        div6.style.color = "white"
+        div5.style.color = "#B5A3D8"
+        div6.style.color = "#B5A3D8"
     }
 });}
+
+if (sobre_mi == true) {
+    const switchButton = document.getElementById("switch");
+    var div1 = document.getElementsByClassName("color1")[0];
+    var div2 = document.getElementsByClassName("color2")[0];
+    switchButton.addEventListener("change", function () {
+    if (this.checked) {
+        div1.style.backgroundColor = "#1E1927";
+        div2.style.backgroundColor = "#352C48";
+        div2.style.boxShadow = "0 0px 10px 5px white"
+    } else {
+        div1.style.backgroundColor = "white";
+        div2.style.backgroundColor = "white";
+        div2.style.boxShadow = "0 0px 10px 5px black";
+    }
+    })
+}
